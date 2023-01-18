8e36496d9f1500723178d730a218adcfd5da9ab2
Serhii Merzhiievskyi <merzhievskiy@gmail.com>
2023-01-18 11:59:44 +0200

    landing done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 window.addEventListener('scroll', e => {
-	document.body.style.cssText += `--scrollTop: ${this.scrollY}px`;
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`)
 })
-
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-ScrollSmoother.create({
-	wrapper: '.wrapper',
-	container: '.container'
-})
+// gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+// ScrollSmoother.create({
+// 	wrapper: '.wrapper',
+// 	content: '.content'
+// })
